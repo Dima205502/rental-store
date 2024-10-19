aca5458595b1a530ef80c9e371e81a7d67364063
Дмитрий Козырев <dmitrijkozyrev183@gmail.com>
2024-10-19 21:58:02 +0300

    delete token

--- utils/send_email.go
+++ utils/send_email.go
@@ -5,7 +5,7 @@ import (
 	"net/smtp"
 )
 
-var sender, appPassword, smtpServer, smtpAdr = "kozyrevdmitrij202@gmail.com", "hzxjahxfyiemidej", "smtp.gmail.com", "smtp.gmail.com:587"
+var sender, appPassword, smtpServer, smtpAdr = "kozyrevdmitrij202@gmail.com", "NoNoNoNo", "smtp.gmail.com", "smtp.gmail.com:587"
 
 func Send(ctx context.Context, email, msg string) error {
 	auth := smtp.PlainAuth("", sender, appPassword, smtpServer)
